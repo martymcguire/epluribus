617c90a9804a6a78407e05aa5afb1a8aaaf4deaf
Marty McGuire <schmartissimo@gmail.com>
2014-01-25 14:24:57 -0500

    Initial Rails 4.0.1 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Epluribus::Application.config.secret_key_base = '4ff319125ea821351773668dd0c17d54c02768314622f2c55bb0b4a61373ff43cbdc5d6811f31ab5e513d60d4630538b0420e1518888094f36eca3803fd0a323'
