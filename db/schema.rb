9055ed07f49b0549d02901f8994590ed709373c1
Marty McGuire <schmartissimo@gmail.com>
2014-01-26 23:21:51 -0500

    Add is_admin flag and filter /admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140126171251) do
+ActiveRecord::Schema.define(version: 20140127041925) do
 
   create_table "parts", force: true do |t|
     t.integer  "project_id"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20140126171251) do
     t.string   "uid"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "is_admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
