98daa07a75504b956d90d187d5464d33b36e32d2
Marty McGuire <schmartissimo@gmail.com>
2014-02-01 13:14:52 -0500

    Add descriptions of state on parts index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -39,4 +39,29 @@ module ApplicationHelper
         "delivered"
     end
   end
+
+  def state_name(state)
+    titles = {
+      'unclaimed' => "Unclaimed Parts",
+      'printing' => "Claimed for Printing",
+      'printed' => "Printed, Awaiting Photo and Measurements",
+      'verification' => "Awaiting Admin Verification",
+      'shipping' => "Ready to Ship",
+      'shipped' => "Shipped",
+      'accepted' => "Received and Accepted"
+    }
+    titles[state] || "Unknown State '#{state}'"
+  end
+
+  def state_description(state)
+    {
+      'unclaimed' => "These parts are waiting for somebody to claim them! Go print more.",
+      'printing' => "These parts have been claimed for printing. If they've been sitting a while, bother the person about it. If it's been a loooong while, Unassign it.",
+      'printed' => "These parts have supposedly been printed and just need to be measured and have a photo taken. If they've been sitting a while, bother the person about it or Unassign it.",
+      'verification' => "These parts are ready to be verified! Check the photo and submitted measurements for sanity and then Verify them! If things look way off, send the person an email and/or Unassign them.",
+      'shipping' => "These parts are verified and ready to ship. If they have been sitting for a while, bother the person. If you're desperate, Unassign the part and have somebody else print it. It's a shame, though, to have the part all printed and verified...",
+      'shipped' => "These parts have supposedly been shipped! Look for them in the mail. Check their tracking info. Whatever.",
+      'accepted' => "These parts have supposedly been received! Now put them together!"
+    }[state] || "Something went wrong."
+  end
 end
