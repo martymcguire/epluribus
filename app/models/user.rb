ce43514e814f6ed52be814fab8d1646cfb0236b6
Marty McGuire <schmartissimo@gmail.com>
2014-07-26 20:51:51 -0400

    User no longer needs a "parts" accessor.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,6 @@ class User < ActiveRecord::Base
   devise :trackable, :rememberable,
          :omniauthable, :omniauth_providers => [:google_oauth2]
 
-  has_many :parts
-
   def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
       data = access_token.info
       user = User.where(:email => data["email"]).first
