20f88f9e6d05bef1ae31426d7a6d61da4fdfefc2
Marty McGuire <schmartissimo@gmail.com>
2014-07-31 15:46:21 -0400

    Attempt to use correct CORS domain for STL preview

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,4 @@
-Paperclip::Attachment.default_options[:url] = "#{ENV['S3_BUCKET_NAME']}.s3.amazonaws.com"
+Paperclip::Attachment.default_options[:url] = ":s3_domain_url"
 Paperclip::Attachment.default_options[:path] = 'uploads/:class/:attachment/:id_partition/:style/:filename'
 
 # nasty hack - stop spoof-checking for STLs because it barfs
