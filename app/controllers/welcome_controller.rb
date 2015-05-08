ca6e27d351e61e6982c9f7646a0bbad0be74ea76
Marty McGuire <schmartissimo@gmail.com>
2015-05-08 16:58:37 -0400

    Show older-than-just-past projects on welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,6 +9,8 @@ class WelcomeController < ApplicationController
       @activity = @project.print_jobs.order('updated_at DESC').limit(8)
     end
     @previous_project = Project.where(complete: true).last
+    pp_id = @previous_project.nil? ? 0 : @previous_project.id
+    @older_projects = Project.where(complete: true).where.not(id: pp_id)
   end
 
   def sign_up
