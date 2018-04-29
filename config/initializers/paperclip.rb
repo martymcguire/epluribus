7377bd60687c7c1c85cbc55e395be72c73e92339
Marty McGuire <schmartissimo@gmail.com>
2018-04-29 15:36:39 -0400

    Enable URLs for Paperclip. dbseed for megarosie.

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,6 +2,8 @@
 Paperclip::Attachment.default_options[:path] = 'uploads/:class/:attachment/:id_partition/:style/:filename'
 Paperclip::Attachment.default_options[:s3_region] = ENV['S3_REGION'] || 'us-east-1'
 
+Paperclip::UriAdapter.register
+
 # nasty hack - stop spoof-checking for STLs because it barfs
 require 'paperclip/media_type_spoof_detector'
 module Paperclip
