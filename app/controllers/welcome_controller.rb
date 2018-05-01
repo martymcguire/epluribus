ccbbf075a2779284c0110f59940de12b886b7754
Marty McGuire <schmartissimo@gmail.com>
2018-04-30 20:42:14 -0400

    redirect homepage to current featured project

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,11 @@ class WelcomeController < ApplicationController
   def index
     @project = Project.where(featured: true).first
     if(@project.nil?)
-      @project = Project.first
+      @project = Project.last
     end
+    # 2018-04-30 - redirect directly to the active project
+    # Disable this after the big NOMCON 2018 push!
+    redirect_to project_path(@project)
   end
 
   def sign_up
