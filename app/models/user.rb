f495fb1e41939bef196530d972454840852d7fc9
Marty McGuire <schmartissimo@gmail.com>
2020-06-25 22:30:17 -0400

    fix sending secondary email confirmations
    after_save checking changed attributes has changed:
    https://www.fastruby.io/blog/rails/upgrades/active-record-5-1-api-changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ApplicationRecord
     :message => 'Must be a valid email address.',
     :allow_blank => true
   before_save :verify_secondary_email
-  after_save :send_secondary_email_verification, :if => :secondary_email_changed?
+  after_save :send_secondary_email_verification, :if => :saved_change_to_secondary_email?
 
   def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
       data = access_token.info
