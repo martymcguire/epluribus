7b862db5606b7489c2ccc09045b05476a080af1e
Marty McGuire <schmartissimo@gmail.com>
2024-11-21 16:17:56 -0500

    rails 8 + app:update
    - bumped sqlite3 gem
    - kept assets config for separate js/css bundles
    - accepted new 8.0 framework defaults
    - seems OK!

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,7 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  ActiveRecord::Migration.check_pending!
+  ActiveRecord::Migration.check_all_pending!
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   #
   # Note: You'll currently still have to declare fixtures explicitly in integration tests
