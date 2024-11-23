282281eb07c271bbbb49ea16b7ef5eba84a44505
Marty McGuire <schmartissimo@gmail.com>
2024-11-22 23:03:37 -0500

    simplify and responsivize layouts, nav
    - drop homepage and how-it-works carousels for thumbnails in the pages
    - add image alts for all those
    - pull those image URLs and the footer links into controllers / helper
      instead of a bunch of repeated markup inline
    - drop old welcome layout so homepage can actually be responsive
    - clean up nav. mobile nav now looks like it!

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,6 +6,40 @@ class WelcomeController < ApplicationController
     end
     # uncomment to redirect directly to the active project
     # redirect_to project_path(@project)
+    @heroes = [
+				{
+          file: 'george-scanning.jpg',
+          alt: 'A man uses a handheld 3D scanner to capture a bust of George Washington'
+        },
+				{
+          file: 'marked-parts.jpg',
+          alt: "Color 3D printed parts with hand-written greetings"
+        },
+				{
+          file: 'patchwork-of-ben-pieces.jpg',
+          alt: 'A grid of builder-submitted photos of their printed parts.'
+        },
+				{
+          file: 'ben-assembly.jpg',
+          alt: 'Tables covered with layers of a partially assemble sculpture.'
+        },
+        {
+          file: 'ben-hat.jpg',
+          alt: 'A person holds the partial sculpture of Ben Franklin over their face'
+        },
+        {
+          file: 'george-complete.jpg',
+          alt: 'Completed sculpture of George Washington with the U.S. Capitol building behind.'
+        },
+        {
+          file: 'ben-complete.jpg',
+          alt: "Completed sculpture of Ben Franklin on a table next to a 3D printer. A laser-cut sign reads 'Ben Franklinstein' and invites viewers to join wethebuilders.com."
+        }
+    ]
+    url_prefix = 'https://s3.amazonaws.com/epluribus/front-rotator/2015-05-16/'
+    @heroes.each do |h|
+      h[:src] = url_prefix + h[:file]
+    end
   end
 
   def sign_up
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,6 +6,17 @@ module ApplicationHelper
      markdown.render(text).html_safe
   end
 
+  def footer_links
+    return [
+      { url: how_it_works_path, label: 'How it Works' },
+      { url: projects_path, label: 'Projects' },
+      { url: 'https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=ZKRTQNJRCFLBA&source=url', label: 'Donate' },
+      { url: sign_up_path, label: 'Join Mailing List' },
+      { url: about_path, label: 'About Us' },
+      { url: 'https://docs.google.com/forms/d/e/1FAIpQLSde8gwwmJRLdCzUeqkqpXH4xJ7_5nSRv2wm0m8CoVPSTQBU5A/viewform?usp=sf_link', label: 'Contact Us' },
+    ]
+  end
+
   def part_preview(part)
     render partial: 'shared/part_preview', locals: { part: part }
   end
