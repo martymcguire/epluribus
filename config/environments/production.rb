b6685b31cdeb8a7625d5a5c1d7d4c3e87b3e68c8
Marty McGuire <schmartissimo@gmail.com>
2024-11-28 10:12:23 -0500

    production: use S3 for ActiveStorage

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
   # config.asset_host = "http://assets.example.com"
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :s3
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   config.assume_ssl = true
